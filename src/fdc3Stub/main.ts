f2ad9b07cfa0f42d99798c78d003f437e55913c4
Nicholas Kolba <nkolba@gmail.com>
2023-03-16 16:18:44 -0400

    cleanup comments

--- src/fdc3Stub/main.ts
+++ src/fdc3Stub/main.ts
@@ -4,23 +4,6 @@ import { sendMessage,  setInstanceId } from './sendMessage';
 import { RegisterInstanceReturn } from '../common/types';
 import { DesktopAgent } from '@finos/fdc3';
 
-//types needed
-//response type (data or error)
-//return handler type
-
-
-
-//find the fdc3 provider and register on startup
-/*document.addEventListener('DOMContentLoaded', async () => {
-    const registrationResult = await sendMessage("registerInstance");
-    const instanceId = (registrationResult.data as RegisterInstanceReturn).instanceId;
-    setInstanceId(instanceId);
-
-    console.log("creating FDC3");
-    window.fdc3 = createAPI();
-    document.dispatchEvent(new CustomEvent('fdc3Ready', {}));
-});
-*/
 
 export const installer = async () : Promise<DesktopAgent> => {
     const registrationResult = await sendMessage("registerInstance");
