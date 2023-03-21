2dab49f20672692607ae6fb21aa379e1b97fcddd
Nicholas Kolba <nkolba@gmail.com>
2023-03-21 08:44:07 -0400

    missing function call

--- src/fdc3Stub/main.ts
+++ src/fdc3Stub/main.ts
@@ -1,11 +1,13 @@
 
-import { createAPI } from './api';
+import { createAPI, setListener } from './api';
 import { sendMessage,  setInstanceId } from './sendMessage';
 import { RegisterInstanceReturn } from '../common/types';
 import { DesktopAgent } from '@finos/fdc3';
 
 
 export const installer = async () : Promise<DesktopAgent> => {
+    setListener();
+    
     const registrationResult = await sendMessage("registerInstance");
     const instanceId = (registrationResult.data as RegisterInstanceReturn).instanceId;
     setInstanceId(instanceId);
